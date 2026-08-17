bb23c9acb0b772eb920759312312377d1c395c21
Mariel Barnett <112442628+marbarn98@users.noreply.github.com>
2026-08-17 14:56:41 -0400

    Qualtrics script and embedded data updates

--- qualtrics.js
+++ qualtrics.js
@@ -33,8 +33,8 @@ Qualtrics.SurveyEngine.addOnload(function()
             var mydata = JSON.parse(event.data.data);
 
 
-            // 1. Leisure Activity Ratings
-            // Pulled in  fixed order as defined in leisure_activities array in MathLeisureChoiceExperiment.js
+            // Leisure activity ratings
+            // Pulled in order from leisure_activities array in MathLeisureChoiceExperiment.js
             var leisure_activities_ordered = [
                 "Read a book", "Do a puzzle", "Cook your favorite meal", "Exercise", "Scroll on social media",
                 "Play your favorite video game", "Hang out with friends", "Grab something to eat", "Watch a show, movie, or video",
@@ -55,8 +55,8 @@ Qualtrics.SurveyEngine.addOnload(function()
             }).toString();
 
 
-            // 2. Math Activity Ratings
-            // Pulled in fixed order as defined in math_assignments array in MathLeisureChoiceExperiment.js
+            // Math activity ratings
+            // Pulled in order from math_assignments array in MathLeisureChoiceExperiment.js
             var math_assignments_ordered = [
                 "Work on a math assignment",  // index 0
                 "Study for a math quiz",      // index 1
@@ -73,14 +73,17 @@ Qualtrics.SurveyEngine.addOnload(function()
             }).toString();
 
 
-            // 3. Top 6 Leisure Activities Ranks (ranked 1st through 6th)
+            // Top 6 leisure activities rankings
             // Read in participant's top 6 rankings from jsPsych data property written by process_survey_data block in MathLeisureChoiceExperiment.js
             var process_trial = mydata.filter(function(trial) { return trial.top_activities; });
             var top_6 = process_trial.length > 0 ? process_trial[0].top_activities : [];
 
 
-            // 4. Choice Task Trial-level Data (216 trials)
+            // Choice task trial-level data 
             var choice_trials = mydata.filter(function(trial) { return trial.phase === '2afc_choice'; });
+
+            // Trial index: keep track of what trials done for a) flagging non-completers/technical issues, and b) converting trial data into long data file later
+            var trial_index = choice_trials.map(function(t, i) { return i + 1; }).toString();
 
             // Which leisure activity shown
             var leisure_option = choice_trials.map(function(t) { return t.leisure_option; }).toString();
@@ -110,46 +113,84 @@ Qualtrics.SurveyEngine.addOnload(function()
             // RT per trial (ms)
             var rt = choice_trials.map(function(t) { return t.rt; }).toString();
 
-            // What was chosen: 'leisure' or 'math'
+            // What category was chosen: 'leisure' or 'math'
             var choice_category = choice_trials.map(function(t) { return t.choice_category; }).toString();
 
         
-            // 5. Summary stats
+            // Summary stats
             var total_trials = choice_trials.length;
 
             var leisure_choices = choice_trials.filter(function(t) { return t.choice_category === 'leisure'; }).length;
             var math_choices = choice_trials.filter(function(t) { return t.choice_category === 'math'; }).length;
 
             var proportion_leisure = total_trials > 0 ? (leisure_choices / total_trials).toFixed(4) : '';
-            var proportion_math = total_trials > 0 ? (math_choices    / total_trials).toFixed(4) : '';
+            var proportion_math = total_trials > 0 ? (math_choices / total_trials).toFixed(4) : '';
 
 
-            // 6. Saving Everything as Qualtrics Embedded Data
+
+            // Calculating deadline sensitivity as a leisure choice difference score: long deadlines vs short
+
+            // First define and record short/long deadline trials
+            var short_deadlines = ["4 hours", "12 hours", "1 day"];
+            var long_deadlines = ["3 days", "1 week", "1 month"];
+
+            var short_deadline_trials = choice_trials.filter(function(t) {
+                return short_deadlines.indexOf(t.math_deadline) !== -1;
+            });
+            var long_deadline_trials = choice_trials.filter(function(t) {
+                return long_deadlines.indexOf(t.math_deadline) !== -1;
+            });
+
+            var short_deadline_leisure_choices = short_deadline_trials.filter(function(t) {
+                return t.choice_category === 'leisure';
+            }).length;
+            var long_deadline_leisure_choices = long_deadline_trials.filter(function(t) {
+                return t.choice_category === 'leisure';
+            }).length;
+
+            var proportion_leisure_short_deadline = short_deadline_trials.length > 0
+                ? (short_deadline_leisure_choices / short_deadline_trials.length) : NaN;
+            var proportion_leisure_long_deadline = long_deadline_trials.length > 0
+                ? (long_deadline_leisure_choices / long_deadline_trials.length) : NaN;
+
+            // Difference score = long-deadline leisure proportion - short
+            // Positive score means ptp is more likely to choose leisure activity (i.e., procrastinate) when the deadline is further out
+            var procrastination_diff_score = (!isNaN(proportion_leisure_short_deadline) && !isNaN(proportion_leisure_long_deadline))
+                ? (proportion_leisure_long_deadline - proportion_leisure_short_deadline).toFixed(4) : '';
+            
+            
+            
+
+            // Saving everything as Qualtrics embedded data
             // Survey ratings
-            Qualtrics.SurveyEngine.setEmbeddedData('leisure_ratings', leisure_ratings);
-            Qualtrics.SurveyEngine.setEmbeddedData('math_ratings', math_ratings);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('leisure_ratings', leisure_ratings);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('math_ratings', math_ratings);
 
             // Top 6 leisure activities (separate variables, ranked 1st to 6th)
             top_6.forEach(function(activity, i) {
-                Qualtrics.SurveyEngine.setEmbeddedData('top_leisure_' + (i + 1), activity);
+                Qualtrics.SurveyEngine.setJSEmbeddedData('top_leisure_' + (i + 1), activity);
             });
 
             // Trial-level choice task data
-            Qualtrics.SurveyEngine.setEmbeddedData('leisure_option', leisure_option);
-            Qualtrics.SurveyEngine.setEmbeddedData('leisure_side', leisure_side);
-            Qualtrics.SurveyEngine.setEmbeddedData('math_option', math_option);
-            Qualtrics.SurveyEngine.setEmbeddedData('math_side', math_side);
-            Qualtrics.SurveyEngine.setEmbeddedData('math_weight', math_weight);
-            Qualtrics.SurveyEngine.setEmbeddedData('math_deadline', math_deadline);
-            Qualtrics.SurveyEngine.setEmbeddedData('rt', rt);
-            Qualtrics.SurveyEngine.setEmbeddedData('choice_category', choice_category);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('trial_index', trial_index);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('leisure_option', leisure_option);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('leisure_side', leisure_side);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('math_option', math_option);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('math_side', math_side);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('math_weight', math_weight);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('math_deadline', math_deadline);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('rt', rt);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('choice_category', choice_category);
 
             // Summary statistics
-            Qualtrics.SurveyEngine.setEmbeddedData('total_trials', total_trials);
-            Qualtrics.SurveyEngine.setEmbeddedData('leisure_choices', leisure_choices);
-            Qualtrics.SurveyEngine.setEmbeddedData('proportion_leisure', proportion_leisure);
-            Qualtrics.SurveyEngine.setEmbeddedData('math_choices', math_choices);
-            Qualtrics.SurveyEngine.setEmbeddedData('proportion_math', proportion_math);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('total_trials', total_trials);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('leisure_choices', leisure_choices);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('proportion_leisure', proportion_leisure);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('math_choices', math_choices);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('proportion_math', proportion_math);
+            Qualtrics.SurveyEngine.setJSEmbeddedData('proportion_leisure_short_deadline', isNaN(proportion_leisure_short_deadline) ? '' : proportion_leisure_short_deadline.toFixed(4));
+            Qualtrics.SurveyEngine.setJSEmbeddedData('proportion_leisure_long_deadline', isNaN(proportion_leisure_long_deadline) ? '' : proportion_leisure_long_deadline.toFixed(4));
+            Qualtrics.SurveyEngine.setJSEmbeddedData('procrastination_diff_score', procrastination_diff_score);
 
             qthis.showNextButton();
             qthis.clickNextButton();
